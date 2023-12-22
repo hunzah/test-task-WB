fe62cc0f6e8ffa3267f289bc2cf404274ab166b4
Yusup <hunzah9@mail.ru>
2023-12-22 18:10:15 +0300

    commit

--- src/header/template/headerTemplate.js
+++ src/header/template/headerTemplate.js
@@ -1,11 +1,10 @@
 import {cards} from "../../state/cards/cards.js";
-import img from './../../../assets/icons/header/burger-icon.svg'
 
 export function headerTemplate() {
     const cardsValue = cards.products.length
     return `
 <span class="header-basket-goods-count-text">${cardsValue}</span>
-<img src="https://hunzah.github.io/test-task-WB/assets/icons/mobile/basket-icon.svg" alt="profile-icon"/>
+<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.zaiushka.com%2F%25D0%25B1%25D1%2583%25D0%25BA%25D0%25B2%25D0%25B0-%25D0%25B1%2F%25D0%25B1%25D0%25B5%25D0%25BB%25D0%25BA%25D0%25B0-%25D0%25BA%25D0%25B0%25D1%2580%25D1%2582%25D0%25B8%25D0%25BD%25D0%25BA%25D0%25B0%2F&psig=AOvVaw34DJ94T__fR-Ldl_mmnFnT&ust=1703344230969000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKDo6-Gpo4MDFQAAAAAdAAAAABAD" alt="profile-icon"/>
 <span class="header-basket-text">Корзина</span>
    `
 }
