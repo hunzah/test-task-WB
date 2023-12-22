59d8ccf7478b903ca664d89aa3b19d8814d7c902
Yusup <hunzah9@mail.ru>
2023-12-22 13:14:37 +0300

    add webpack

--- main.js
+++ main.js
@@ -1,53 +1,10 @@
-import {total} from '/src/state/total/total.js'
-
-function toggleContentVisibility2() {
-    const accordionButton = document.getElementById('not-available-accordion-arrow-button');
-    const cardsContainer = document.getElementById('not-available-prod-cards-list');
-    if (accordionButton && cardsContainer) {
-        accordionButton.addEventListener('click', function () {
-            if (cardsContainer.style.display === 'none') {
-                cardsContainer.style.display = 'flex';
-                accordionButton.classList.remove('rotate');
-            } else {
-                cardsContainer.style.display = 'none';
-                accordionButton.classList.add('rotate');
-            }
-        });
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-
-
-    toggleContentVisibility2();
-});
-
-function toggleContentVisibility() {
-    const accordionButton = document.getElementById('accordion-arrow-button');
-    const cardsContainer = document.getElementById('cards-list');
-    const checkbox = document.querySelector('.pick-all-checkbox-container');
-
-
-    if (accordionButton && cardsContainer && checkbox) {
-        accordionButton.addEventListener('click', function () {
-            if (cardsContainer.style.display === 'none') {
-                cardsContainer.style.display = 'flex';
-
-                checkbox.innerHTML = '<input checked type="checkbox" class="checkbox" id="choose-all-checkbox">' +
-                    '<label for="choose-all-checkbox" class="custom-checkbox-label"></label>' +
-                    '<span class="choose-all-checkbox-text">Выбрать все</span>';
-                accordionButton.classList.remove('rotate');
-            } else {
-                cardsContainer.style.display = 'none';
-                checkbox.innerHTML = `<span>${total.totalProducts} товара · ${total.totalAmount} ${total.currency}</span>`;
-                checkbox.classList.add('out-of-stock-text');
-                accordionButton.classList.add('rotate');
-            }
-        });
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    toggleContentVisibility();
-});
+import './src/product-cards/logic/toggleContentVisibility.js';
+import './src/header/header.js';
+import './src/product-cards/productCards.js';
+import './src/product-cards/not-available-product-cards.js';
+import './src/total/total.js';
+import './src/pay-method/payMethod.js';
+import './src/delivery-method/deliveryMethod.js';
+import './src/modals/change-pay-method/changePayMethod.js';
+import './src/modals/change-pick-up-address/changePickUpPoint.js';
 
