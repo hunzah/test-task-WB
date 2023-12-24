35d54076ae1b934c1ab1a1e66faf14df6c36ea07
Yusup <hunzah9@mail.ru>
2023-12-24 16:06:35 +0300

    debug

--- src/form/form.js
+++ src/form/form.js
@@ -36,22 +36,20 @@ const validateInput = (input, error, validationRegex, errorMessage) => {
 }
 const nameInput = document.querySelector('.receiver-name-input');
 const nameError = document.querySelector('.receiver-input-error');
-
 validateInput(nameInput, nameError, /^[a-zA-Zа-яА-Я]+$/, 'Имя может содержать только буквы');
 
-
 const lastNameInput = document.querySelector('.receiver-lastname-input');
-const  lastNameError = document.querySelector('.receiver-lastname-input-error');
+const lastNameError = document.querySelector('.receiver-lastname-input-error');
 validateInput(lastNameInput, lastNameError, /^[a-zA-Zа-яА-Я]+$/, 'Фамилия может содержать только буквы');
 
 const phoneInput = document.querySelector('.receiver-phone-input');
 const phoneError = document.querySelector('.receiver-phone-input-error');
-validateInput(phoneInput, phoneError, /^\+?[0-9]+$/, 'Веедите корректный номер');
+validateInput(phoneInput, phoneError, /^\+?[0-9]+$/, 'Введите корректный номер');
 
 const mailInput = document.querySelector('.receiver-email-input');
 const mailError = document.querySelector('.receiver-email-input-error');
-validateInput(mailInput, mailError, /^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Веедите корректный email');
+validateInput(mailInput, mailError, /^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Введите корректный email');
 
 const innInput = document.querySelector('.receiver-INN-input');
-const innError = document.querySelector('.receiver-input-text');
-validateInput(innInput, innError, /^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Веедите корректный ИНН');
+const innError = document.querySelector('.receiver-INN-error');
+validateInput(innInput, innError, /^\d+$/, 'Введите корректный ИНН');
